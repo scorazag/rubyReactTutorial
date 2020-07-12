c6cebd595f6cfd47fc5391446a5e81d5aa5d0b6a
Sergio Coraza <scorazag0@gmail.com>
2020-07-11 20:12:42 -0500

    Falta el update

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -1,10 +1,55 @@
 // app/javascript/packs/components/TodoApp.jsx
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+import TodoItems from './TodoItems'
+import TodoItem from './TodoItem'
+import TodoForm from './TodoForm'
 
 class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItems: []
+    };
+    this.getTodoItems = this.getTodoItems.bind(this);
+    this.createTodoItem = this.createTodoItem.bind(this);
+  }
+  componentDidMount() {
+    this.getTodoItems();
+  }
+  //aqui se hace la peticion a la api
+  getTodoItems() {
+    axios
+      .get("/api/v1/todo_items")
+      .then(response => {
+        const todoItems = response.data;
+        this.setState({ todoItems });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  createTodoItem(todoItem) {
+    const todoItems = [todoItem, ...this.state.todoItems];
+    this.setState({ todoItems });
+  }
   render() {
-    return <p>TodoApp</p>
+    return (
+      <>
+        <TodoForm createTodoItem={this.createTodoItem} />
+        <TodoItems>
+          {this.state.todoItems.map(todoItem => (
+            <TodoItem
+              key={todoItem.id}
+              todoItem={todoItem}
+              getTodoItems={this.getTodoItems}
+            />
+          ))}
+        </TodoItems>
+      </>
+    );
   }
 }
 
--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -2,11 +2,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import axios from "axios";
+import setAxiosHeaders from "./AxiosHeaders";
+
 class TodoItem extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       complete: this.props.todoItem.complete,
+    };
+    this.handleDestroy = this.handleDestroy.bind(this);
+    this.path = `/api/v1/todo_items/${this.props.todoItem.id}`;
+  }
+  handleDestroy() {
+    setAxiosHeaders();
+    const confirmation = confirm("Are you sure?");
+    if (confirmation) {
+      axios
+        .delete(this.path)
+        .then(response => {
+          this.props.getTodoItems();
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
   render() {
@@ -61,7 +80,11 @@ class TodoItem extends React.Component {
               Complete?
             </label>
           </div>
-          <button className="btn btn-outline-danger">Delete</button>
+          <button
+            onClick={this.handleDestroy}
+            className="btn btn-outline-danger">
+            Delete
+          </button>
         </td>
       </tr>
     )
@@ -72,4 +95,5 @@ export default TodoItem
 
 TodoItem.propTypes = {
   todoItem: PropTypes.object.isRequired,
+  getTodoItems: PropTypes.func.isRequired
 }
